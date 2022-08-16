96ed488f5b06f4e26433c1b2cc60346a4d490685
Callum Owen <callumowen6@googlemail.com>
2022-08-16 22:07:32 +0100

    Instantiating Enemies in Object Pool

---
+++ Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+	[SerializeField] GameObject enemy;
+	[SerializeField] int poolSize = 5;
+	[SerializeField] float spawnTimer = 1f;
+
+	GameObject[] pool;
+
+	void Awake()
+	{
+		PopulatePool();
+	}
+
+	void Start()
+	{
+		StartCoroutine(SpawnEnemy());
+	}
+
+	void PopulatePool()
+	{
+		pool = new GameObject[poolSize];
+
+		for (int i = 0; i < poolSize; i++)
+		{
+			pool[i] = Instantiate(enemy, transform);
+			pool[i].SetActive(false);
+		}
+	}
+
+	void EnableObjectInPool()
+	{
+		for (int i = 0; i < poolSize; i++)
+		{
+			if (!pool[i].activeInHierarchy)
+			{
+				pool[i].SetActive(true);
+				return;
+			}
+		}
+	}
+
+	IEnumerator SpawnEnemy()
+	{
+		while (true)
+		{
+			EnableObjectInPool();
+			yield return new WaitForSeconds(spawnTimer);
+		}
+	}
+}
